1948caa95d7d6325431fb777a6c7e1e7682e52f3
Eddiebee <edbeeotudor@gmail.com>
2022-11-02 11:20:52 +0100

    Feature: show specific validation errors

--- components/UserForm.tsx
+++ components/UserForm.tsx
@@ -1,14 +1,35 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const UserForm = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    let emailValid = false;
+    // some basic validation
+    if (email.length === 0) {
+      setEmailError("Email is required");
+    } else if (email.length < 6) {
+      setEmailError("Email should be minimum 6 characters");
+    } else if (email.indexOf(" ") >= 0) {
+      setEmailError("Email cannot contain spaces");
+    } else {
+      setEmailError("");
+      emailValid = true;
+    }
+    if (emailValid) {
+      alert("Email: " + email + "\nPassword: " + password);
+    }
+  };
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control
