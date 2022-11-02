c5ff84da50fae67c1dfc6e883812e6f152cd85fe
Eddiebee <edbeeotudor@gmail.com>
2022-11-02 10:44:42 +0100

    Add/modify the Form component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>UserFrom</title>
+        <title>UserForm</title>
         <meta
           name="description"
           content="Built with React Hooks, React-Bootstrap, TS..."
@@ -14,7 +17,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </main>
 
       <footer className={styles.footer}>
         <a
